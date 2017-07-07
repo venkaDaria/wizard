ecea34f9159eec1aecc6dc94a88017b1b3966578
Daria Pydorenko <venka.daria@gmail.com>
2017-07-07 11:59:08 +0300

    edit session

--- src/app/util/main.component.js
+++ src/app/util/main.component.js
@@ -1,45 +1,20 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var router_1 = require("@angular/router");
-var core_1 = require("@angular/core");
 var session_1 = require("./session");
 var MainComponent = (function () {
-    function MainComponent(router) {
-        this.router = router;
+    function MainComponent() {
     }
-    MainComponent.prototype.go_next = function (param) {
-        console.log(session_1.Session);
-        console.log(param);
-        console.log(this.get(param));
-        session_1.Session.set(param, this.get(param));
+    MainComponent.prototype.save = function (key, value) {
+        session_1.Session.set(key, value);
     };
-    MainComponent.prototype.get = function (param) {
-        console.log(this.router);
-        return this.router.parseUrl(this.router.url).queryParamMap.get(param);
-    };
-    MainComponent.go_retry = function () {
+    MainComponent.prototype.clear = function () {
         session_1.Session.clear();
     };
-    MainComponent.get_value = function (param) {
-        console.log(session_1.Session);
-        console.log(param);
-        console.log(session_1.Session.get(param));
-        return session_1.Session.get(param);
+    MainComponent.prototype.get_value = function (key) {
+        console.log("v: " + session_1.Session.get(key));
+        return session_1.Session.get(key);
     };
     return MainComponent;
 }());
-MainComponent = __decorate([
-    core_1.Injectable(),
-    __metadata("design:paramtypes", [router_1.Router])
-], MainComponent);
 exports.MainComponent = MainComponent;
 //# sourceMappingURL=main.component.js.map
--- src/app/util/main.component.ts
+++ src/app/util/main.component.ts
@@ -1,32 +1,19 @@
-import { Router } from "@angular/router";
-import {Injectable} from "@angular/core";
 import {Session} from "./session";
 
-@Injectable()
 export class MainComponent {
 
-    constructor(private router: Router){}
+    constructor(){}
 
-    go_next(param: string) {
-        console.log(Session);
-        console.log(param);
-        console.log(this.get(param));
-        Session.set(param, this.get(param));
+    save(key: string, value: string) {
+        Session.set(key, value);
     }
 
-    get(param: string) : string {
-        console.log(this.router);
-        return this.router.parseUrl(this.router.url).queryParamMap.get(param);
-    }
-
-    static go_retry() {
+    clear() {
         Session.clear();
     }
 
-    static get_value(param: string) : string {
-        console.log(Session);
-        console.log(param);
-        console.log(Session.get(param));
-        return Session.get(param);
+    get_value(key: string) : string {
+        console.log("v: " + Session.get(key));
+        return Session.get(key);
     }
 }
